3742c50a7b691a3f907e75076a23b1338fc633f7
RyCab <rcabral0805@gmail.com>
2024-03-18 14:46:13 -0400

    img

--- client/src/components/ReleaseCalendar/ReleasingProduct/ReleasingProduct.jsx
+++ client/src/components/ReleaseCalendar/ReleasingProduct/ReleasingProduct.jsx
@@ -5,6 +5,7 @@ const ReleasingProduct = ({ product }) => {
     console.log(product);
     return product ? (
         <div>
+            <img src={product.images[0]}></img>
             <h3>{product.title}</h3>
             <p>{product.description}</p>
         </div>
